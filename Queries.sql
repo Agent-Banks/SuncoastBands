396a59813b20b3a67b0baf3e8941fc276c1f384a
CodyBanks <codydouglasbanks@gmail.com>
2020-05-28 14:45:06 -0400

    First Table Fininshed

---
+++ Queries.sql
@@ -0,0 +1,16 @@
+--1.
+
+CREATE TABLE "BANDS"
+(
+  "Id" SERIAL PRIMARY KEY,
+  "Name" TEXT,
+  "CountryOfOrign" TEXT,
+  "NumberOfMemebers" TEXT,
+  "Website" TEXT,
+  "Style" TEXT,
+  "IsSigned" TEXT,
+  "ContractName" TEXT,
+  "ContractPhoneNumber" TEXT
+)
+
+CREATE TABLE 
