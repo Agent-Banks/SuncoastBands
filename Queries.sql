38394c7c9c131d2b967728434d722cdd7f1ee279
CodyBanks <codydouglasbanks@gmail.com>
2020-05-28 15:14:48 -0400

    Small fix

--- Queries.sql
+++ Queries.sql
@@ -3,14 +3,28 @@
 CREATE TABLE "BANDS"
 (
   "Id" SERIAL PRIMARY KEY,
-  "Name" TEXT,
+  "Name" TEXT NOT NULL,
   "CountryOfOrign" TEXT,
-  "NumberOfMemebers" TEXT,
+  "NumberOfMemebers" INT,
   "Website" TEXT,
   "Style" TEXT,
-  "IsSigned" TEXT,
+  "IsSigned" BOOLEAN,
   "ContractName" TEXT,
   "ContractPhoneNumber" TEXT
 )
 
-CREATE TABLE 
+CREATE TABLE "Albums"
+(
+  "Id" SERIAL PRIMARY KEY,
+  "BandId" INTEGER REFERENCES "BANDS" ("Id"),
+  "Title" TEXT NOT NULL,
+  "IsExplicit" BOOLEAN,
+  "ReleaseDate" TIMESTAMP,
+)
+
+--2. 
+
+INSERT INTO "BANDS"
+  ( "Name", "CountryOfOrign", "NumberOfMemebers", "Website", "Style", "IsSigned", "ContractName", "ContractPhoneNumber")
+VALUES
+  ()
